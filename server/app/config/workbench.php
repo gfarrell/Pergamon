3c4d15c80f080de2652e48e09cb62d29d89bead6
Gideon Farrell <me@gideonfarrell.co.uk>
2013-07-28 23:28:47 +0200

    Updated workbench.php confi.
    Signed-off-by: Gideon Farrell <me@gideonfarrell.co.uk>

--- server/app/config/workbench.php
+++ server/app/config/workbench.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'name' => '',
+	'name' => 'Gideon Farrell',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -26,6 +26,6 @@ return array(
 	|
 	*/
 
-	'email' => '',
+	'email' => 'me@gideonfarrell.co.uk',
 
 );
