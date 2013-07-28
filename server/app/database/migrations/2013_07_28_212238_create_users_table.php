c5edeaf60f0dfd51c10cc6302d6296ad529eb2fd
Gideon Farrell <me@gideonfarrell.co.uk>
2013-07-28 23:28:57 +0200

    [Migration] Create a user table
    Signed-off-by: Gideon Farrell <me@gideonfarrell.co.uk>

---
+++ server/app/database/migrations/2013_07_28_212238_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Create the users table
+		Schema::create('users', function($table){
+			$table->increments('id');
+			$table->string('email');
+			$table->string('first_name');
+			$table->string('last_name');
+			
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Delete the users table
+		Schema::drop('users');
+	}
+
+}
