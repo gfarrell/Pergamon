721db7625033047ec89e9b93c52d880aa30443a3
Gideon Farrell <me@gideonfarrell.co.uk>
2013-07-28 23:39:33 +0200

    Added a password field to the users table migration
    Signed-off-by: Gideon Farrell <me@gideonfarrell.co.uk>

--- server/app/database/migrations/2013_07_28_212238_create_users_table.php
+++ server/app/database/migrations/2013_07_28_212238_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration {
 			$table->string('email');
 			$table->string('first_name');
 			$table->string('last_name');
+
+			$table->string('password');
 			
 			$table->timestamps();
 		});
