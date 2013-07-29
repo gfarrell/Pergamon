d681f150f04e8e39edc80d35dce6a790a2c0adec
Gideon Farrell <me@gideonfarrell.co.uk>
2013-07-29 08:34:08 +0200

    Added methods to relate MediaObjects to Shelves
    Signed-off-by: Gideon Farrell <me@gideonfarrell.co.uk>

--- server/app/models/AbstractMediaObject.php
+++ server/app/models/AbstractMediaObject.php
@@ -1,5 +1,26 @@
 <?php
 
 abstract class AbstractMediaObject extends Eloquent {
+    /**
+     * Gets the shelf (location) of the media object.
+     * @return Shelf
+     */
+    public function shelf() {
+        return $this->belongsTo('shelf');
+    }
 
+    /**
+     * Moved a MediaObject to a new, specified shelf.
+     * @param  int    $new_shelf_id the ID of the shelf to be moved to
+     * @return void
+     */
+    public function moveToShelf(int $new_shelf_id) {
+        try {
+            // Check if the new shelf exists
+            $shelf = Shelf::findOrFail($new_shelf_id);
+            $this->shelf()->associate($shelf);
+        } catch(ModelNotFoundException $e) {
+            // Warn the user that the shelf they are trying to move to doesn't exist
+        } // don't do anything else if there is an error
+    }
 }
